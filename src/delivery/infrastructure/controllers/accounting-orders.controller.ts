8523e9feb90c4cf55dc645e7dbefa922db66b69e
zhuravlevma <monge.gaspard.1746@gmail.com>
2023-04-27 12:25:56 +0300

    fix: fix import

--- src/delivery/infrastructure/controllers/accounting-orders.controller.ts
+++ src/delivery/infrastructure/controllers/accounting-orders.controller.ts
@@ -1,7 +1,6 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UpdateOrderNestDto } from '../dtos/update-orders.dto';
 import { AccountingOrderEntity } from 'src/delivery/domain/accounting-order/entities/accounting-order.entity';
-import { FindAllOrdersPort } from 'src/delivery/domain/accounting-order/ports/out/find-all-orders.port';
 import { FindAllOrdersUseCase } from 'src/delivery/domain/accounting-order/ports/in/find-all-orders.use-case';
 import { UpdateOrderUseCase } from 'src/delivery/domain/accounting-order/ports/in/update-order.use-case';
 
