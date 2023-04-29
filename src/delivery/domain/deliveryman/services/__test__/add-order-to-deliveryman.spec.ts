89aeb359e67e44d60593ec298bd42fd83414428c
zhuravlevma <monge.gaspard.1746@gmail.com>
2023-04-29 23:41:27 +0300

    test: fix test

--- src/delivery/domain/deliveryman/services/__test__/add-order-to-deliveryman.spec.ts
+++ src/delivery/domain/deliveryman/services/__test__/add-order-to-deliveryman.spec.ts
@@ -1,32 +1,40 @@
 import { DeliverymanEntity } from '../../entities/deliveryman.entity';
+import { AddOrderToDeliverymanDto } from '../../ports/in/add-order-to-deliveryman.use-case';
 import { AddOrderToDeliverymanService } from '../add-order-to-deliveryman.service';
 
 describe('delivery/domain/deliveryman/services/add-order-to-deliveryman.service', () => {
-  it('adding order is complete', async () => {
-    const deliverymanEntity = new DeliverymanEntity({
+  let deliverymanEntity: DeliverymanEntity;
+  const saveDeliverymanPort = {
+    save: jest.fn(),
+  };
+  const findDeliverymanByIdWithOrdersPort = {
+    findDeliverymanByIdWithOrders: jest.fn(),
+  };
+
+  beforeEach(() => {
+    deliverymanEntity = new DeliverymanEntity({
       id: 'uuid',
       firstName: 'test_name',
       lastName: 'test_lastname',
       isActive: true,
       orders: [],
     });
-    const findDeliverymanByIdWithOrdersPort = {
-      findDeliverymanByIdWithOrders: jest
-        .fn()
-        .mockResolvedValue(deliverymanEntity),
-    };
-    const saveDeliverymanPort = {
-      save: jest.fn(),
-    };
+  });
+  it('adding order is complete', async () => {
+    findDeliverymanByIdWithOrdersPort.findDeliverymanByIdWithOrders.mockResolvedValue(
+      deliverymanEntity,
+    );
     const service = new AddOrderToDeliverymanService(
       findDeliverymanByIdWithOrdersPort,
       saveDeliverymanPort,
     );
 
-    await service.addOrderToDeliveryman('deliverymanId', {
+    const createOrderDto: AddOrderToDeliverymanDto = {
       name: 'test',
       description: 'testdescr',
-    });
+    };
+
+    await service.addOrderToDeliveryman('deliverymanId', createOrderDto);
 
     expect(deliverymanEntity.orders.length).toEqual(1);
   });
