d296c04a2449d36c6426377a32f23a6caa6a7fb7
zhuravlevma <monge.gaspard.1746@gmail.com>
2024-02-16 19:01:38 +0300

    fix: fix dtos

--- src/accounting/verification/dtos/update-verification.dto.ts
+++ src/accounting/verification/dtos/update-verification.dto.ts
@@ -1,6 +1,18 @@
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
+
 export class UpdateVerificationDto {
+  @IsString()
   id: string;
+
+  @IsBoolean()
+  @IsOptional()
   isFull?: boolean;
+
+  @IsBoolean()
+  @IsOptional()
   completed?: boolean;
+
+  @IsBoolean()
+  @IsOptional()
   signed?: boolean;
 }
