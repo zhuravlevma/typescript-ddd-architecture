62c45441b33d69a7189fe9555d6c37c7247720e8
zhuravlevma <monge.gaspard.1746@gmail.com>
2024-03-16 23:41:09 +0300

    fix: fix ports

--- src/delivery/curiers/curier/domain/read-models/curier-with-count-of-orders.model.ts
+++ src/delivery/curiers/curier/domain/read-models/curier-with-count-of-orders.model.ts
@@ -1,4 +1,16 @@
-export class CuriernWithCountOfOrders {
+interface CuriernWithCountOfOrdersAttributes {
   curierId: string;
   count: number;
+}
+
+export class CuriernWithCountOfOrders
+  implements CuriernWithCountOfOrdersAttributes
+{
+  readonly curierId: string;
+  readonly count: number;
+
+  constructor(attributes: CuriernWithCountOfOrdersAttributes) {
+    this.curierId = attributes.curierId;
+    this.count = attributes.count;
+  }
 }
--- src/delivery/curiers/curier/domain/read-models/order.model.ts
+++ src/delivery/curiers/curier/domain/read-models/order.model.ts
@@ -1,8 +1,26 @@
-export class OrderModel {
+interface OrderAttributes {
   id: string;
   name: string;
   description: string;
   isActive: boolean;
   curierId: string;
   orderId: string;
+}
+
+export class OrderModel implements OrderAttributes {
+  readonly id: string;
+  readonly name: string;
+  readonly description: string;
+  readonly isActive: boolean;
+  readonly curierId: string;
+  readonly orderId: string;
+
+  constructor(attributes: OrderAttributes) {
+    this.id = attributes.id;
+    this.name = attributes.name;
+    this.description = attributes.description;
+    this.isActive = attributes.isActive;
+    this.curierId = attributes.curierId;
+    this.orderId = attributes.orderId;
+  }
 }
