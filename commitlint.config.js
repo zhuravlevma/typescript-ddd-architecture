16b7b8ace4e66857152c0932bb0a76fd2aa33a03
zhuravlevma <monge.gaspard.1746@gmail.com>
2024-12-10 22:50:44 +0300

    fix: fix config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,22 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'header-max-length': [2, 'always', 72],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'chore',
+        'style',
+        'refactor',
+        'ci',
+        'test',
+        'revert',
+        'perf',
+        'vercel',
+      ],
+    ],
   },
 };
